54caad010569aa1007fa20d11f05757e73bf524f
Muhammed Shaheen ck <107412376+shahck@users.noreply.github.com>
2022-11-30 21:30:31 +0530

    Update settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dr%x3^9+s0=&cl=$ty3s1zjrg5-jkq%ht0=7z%^q(92&3zj0b%'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,13 +85,7 @@ AUTH_USER_MODEL = 'accounts.Accounts'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'w',
-       'USER': 'postgres',
-       'PASSWORD': 'shaheen',
-       'HOST': 'localhost',
-       
+   
    }
 }
 
